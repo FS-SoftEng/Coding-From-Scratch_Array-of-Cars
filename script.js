5111c2cad4c459208c4449f19ba8cf79ab562b7a
Ian Ditsch <IanDitsch@Outlook.com>
2019-07-05 22:22:56 -0500

    Coding From Scratch JS Array of Cars Activity

---
+++ script.js
@@ -0,0 +1,10 @@
+var amazingCars = ["BMW", "Lexus", "Ford", "Mercedes",]; 
+console.log(amazingCars);
+
+amazingCars.pop();
+console.log(amazingCars);
+
+amazingCars.sort();
+console.log(amazingCars);
+
+console.log(amazingCars.length); 
